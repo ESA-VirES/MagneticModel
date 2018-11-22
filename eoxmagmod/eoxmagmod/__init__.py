2c3408fd6c6903e2c933f763a29360d161389586
Martin Paces <martin.paces@eox.at>
2018-11-22 13:50:09 +0100

    Incrementing version.

--- eoxmagmod/eoxmagmod/__init__.py
+++ eoxmagmod/eoxmagmod/__init__.py
@@ -133,7 +133,7 @@ __all__ = [
     'trace_field_line',
 ]
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __author__ = 'Martin Paces (martin.paces@eox.at)'
 __copyright__ = 'Copyright (C) 2014 EOX IT Services GmbH'
 __licence__ = 'EOX licence (MIT style)'
