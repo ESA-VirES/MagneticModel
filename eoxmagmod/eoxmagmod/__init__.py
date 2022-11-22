3517b4e73adef347a127b30da90ceae6278c89ed
Martin Paces <martin.paces@eox.at>
2022-11-22 22:41:06 +0100

    Version bump (0.11.0)

--- eoxmagmod/eoxmagmod/__init__.py
+++ eoxmagmod/eoxmagmod/__init__.py
@@ -140,7 +140,7 @@ try:
 except ImportError:
     pass
 
-__version__ = '0.11.0rc1'
+__version__ = '0.11.0'
 __author__ = 'Martin Paces (martin.paces@eox.at)'
 __copyright__ = 'Copyright (C) 2014-2022 EOX IT Services GmbH'
 __licence__ = 'EOX licence (MIT style)'
