ff5e81ef4c1676e8eb82ceeff5a45835dd54888a
Martin Paces <martin.paces@eox.at>
2022-12-08 19:19:56 +0100

    Merging multiple MMA model files.

--- eoxmagmod/eoxmagmod/magnetic_model/loader_mma.py
+++ eoxmagmod/eoxmagmod/magnetic_model/loader_mma.py
@@ -28,6 +28,7 @@
 # pylint: disable=invalid-name
 
 from spacepy import pycdf
+from numpy import concatenate, array_equal
 from .model import (
     SphericalHarmomicGeomagneticModel,
     DipoleSphericalHarmomicGeomagneticModel,
@@ -46,120 +47,124 @@ MMA2C_NGP_LATITUDE = 90 - 9.92   # deg.
 MMA2C_NGP_LONGITUDE = 287.78 - 360.0  # deg.
 
 
-def load_model_swarm_mma_2c_internal(path, lat_ngp=MMA2C_NGP_LATITUDE,
+def load_model_swarm_mma_2c_internal(*paths, lat_ngp=MMA2C_NGP_LATITUDE,
                                      lon_ngp=MMA2C_NGP_LONGITUDE):
     """ Load internal (secondary field) model from a Swarm MMA_SHA_2C product.
     """
     return DipoleSphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2c_internal(path),
+        load_coeff_swarm_mma_2c_internal(*paths),
         north_pole=(lat_ngp, lon_ngp),
     )
 
 
-def load_model_swarm_mma_2c_external(path, lat_ngp=MMA2C_NGP_LATITUDE,
+def load_model_swarm_mma_2c_external(*paths, lat_ngp=MMA2C_NGP_LATITUDE,
                                      lon_ngp=MMA2C_NGP_LONGITUDE):
     """ Load external (primary field) model from a Swarm MMA_SHA_2C product.
     """
     return DipoleSphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2c_external(path),
+        load_coeff_swarm_mma_2c_external(*paths),
         north_pole=(lat_ngp, lon_ngp),
     )
 
 
-def load_model_swarm_mma_2f_geo_internal(path):
+def load_model_swarm_mma_2f_geo_internal(*paths):
     """ Load geographic frame internal (secondary field) model from a Swarm
     MMA_SHA_2F product.
     """
     return SphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2f_geo_internal(path)
+        load_coeff_swarm_mma_2f_geo_internal(*paths)
     )
 
 
-def load_model_swarm_mma_2f_geo_external(path):
+def load_model_swarm_mma_2f_geo_external(*paths):
     """ Load geographic frame internal (primary field) model from a Swarm
     MMA_SHA_2F product.
     """
     return SphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2f_geo_external(path)
+        load_coeff_swarm_mma_2f_geo_external(*paths)
     )
 
 
-def load_model_swarm_mma_2f_sm_internal(path):
+def load_model_swarm_mma_2f_sm_internal(*paths):
     """ Load solar magnetic frame internal (secondary field) model from a Swarm
     MMA_SHA_2F product.
     """
     # FIXME: solar-magnetic frame model
     return SphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2f_sm_internal(path),
+        load_coeff_swarm_mma_2f_sm_internal(*paths),
     )
 
 
-def load_model_swarm_mma_2f_sm_external(path):
+def load_model_swarm_mma_2f_sm_external(*paths):
     """ Load solar magnetic frame internal (primary field) model from a Swarm
     MMA_SHA_2F product.
     """
     # FIXME: solar-magnetic frame model
     return SphericalHarmomicGeomagneticModel(
-        load_coeff_swarm_mma_2f_sm_external(path)
+        load_coeff_swarm_mma_2f_sm_external(*paths)
     )
 
 
-def load_coeff_swarm_mma_2c_internal(path):
-    """ Load internal model coefficients from a Swarm MMA_SHA_2C product file.
+def load_coeff_swarm_mma_2c_internal(*paths):
+    """ Load internal model coefficients from one or more Swarm MMA_SHA_2C product files.
+    Note that the models are loaded and merged in the order of the arguments.
     """
     return _load_coeff_mma_multi_set(
-        path, read_swarm_mma_2c_internal, "gh", is_internal=True
+        paths, read_swarm_mma_2c_internal, "gh", is_internal=True
     )
 
 
-def load_coeff_swarm_mma_2c_external(path):
-    """ Load external model coefficients from a Swarm MMA_SHA_2C product file.
+def load_coeff_swarm_mma_2c_external(*paths):
+    """ Load external model coefficients from one or more Swarm MMA_SHA_2C product files.
+    Note that the models are loaded and merged in the order of the arguments.
     """
     return _load_coeff_mma_multi_set(
-        path, read_swarm_mma_2c_external, "qs", is_internal=False
+        paths, read_swarm_mma_2c_external, "qs", is_internal=False
     )
 
 
-def load_coeff_swarm_mma_2f_geo_internal(path):
+def load_coeff_swarm_mma_2f_geo_internal(*paths):
     """ Load internal geographic frame model coefficients from a Swarm
     MMA_SHA_2F product file.
     """
     return _load_coeff_mma_single_set(
-        path, read_swarm_mma_2f_geo_internal, "gh", is_internal=True
+        paths, read_swarm_mma_2f_geo_internal, "gh", is_internal=True
     )
 
 
-def load_coeff_swarm_mma_2f_geo_external(path):
+def load_coeff_swarm_mma_2f_geo_external(*paths):
     """ Load external geographic frame model coefficients from a Swarm
     MMA_SHA_2F product file.
     """
     return _load_coeff_mma_single_set(
-        path, read_swarm_mma_2f_geo_external, "qs", is_internal=False
+        paths, read_swarm_mma_2f_geo_external, "qs", is_internal=False
     )
 
 
-def load_coeff_swarm_mma_2f_sm_internal(path):
+def load_coeff_swarm_mma_2f_sm_internal(*paths):
     """ Load internal solar magnetic frame model coefficients from a Swarm
     MMA_SHA_2F product file.
     """
     return _load_coeff_mma_single_set(
-        path, read_swarm_mma_2f_sm_internal, "gh", is_internal=True
+        paths, read_swarm_mma_2f_sm_internal, "gh", is_internal=True
     )
 
 
-def load_coeff_swarm_mma_2f_sm_external(path):
+def load_coeff_swarm_mma_2f_sm_external(*paths):
     """ Load external solar magnetic frame model coefficients from a Swarm
     MMA_SHA_2F product file.
     """
     return _load_coeff_mma_single_set(
-        path, read_swarm_mma_2f_sm_external, "qs", is_internal=False
+        paths, read_swarm_mma_2f_sm_external, "qs", is_internal=False
     )
 
 
-def _load_coeff_mma_multi_set(path, cdf_reader, variable, is_internal):
-    with pycdf.CDF(path) as cdf:
-        data = cdf_reader(cdf)
-
+def _load_coeff_mma_multi_set(paths, cdf_reader, variable, is_internal):
+    data = [_read_data(path, cdf_reader) for path in paths]
+    data = [
+        _merge_coefficients(*items, variable=variable)
+        for items in zip(*data)
+    ]
     return CombinedSHCoefficients(*[
         SparseSHCoefficientsTimeDependent(
             item["nm"], item[variable], item["t"], is_internal=is_internal
@@ -167,10 +172,40 @@ def _load_coeff_mma_multi_set(path, cdf_reader, variable, is_internal):
     ])
 
 
-def _load_coeff_mma_single_set(path, cdf_reader, variable, is_internal):
-    with pycdf.CDF(path) as cdf:
-        data = cdf_reader(cdf)
-
+def _load_coeff_mma_single_set(paths, cdf_reader, variable, is_internal):
+    data = [_read_data(path, cdf_reader) for path in paths]
+    data = _merge_coefficients(*data, variable=variable)
     return SparseSHCoefficientsTimeDependent(
         data["nm"], data[variable], data["t"], is_internal=is_internal
     )
+
+
+def _read_data(path, cdf_reader):
+    with pycdf.CDF(path) as cdf:
+        return cdf_reader(cdf)
+
+
+def _merge_coefficients(*sets, variable):
+    head, *tail = sets
+
+    # sanity check
+    previous_item = head
+    for item in tail:
+        if (
+            previous_item["degree_min"] != item["degree_min"] or
+            previous_item["degree_max"] != item["degree_max"]
+        ):
+            raise ValueError("Degree range mismatch!")
+        if not array_equal(previous_item["nm"], item["nm"]):
+            raise ValueError("Incompatible sets of coefficients!")
+        if previous_item["t"][-1] > item["t"][0]:
+            raise ValueError("Unordered sets of coefficients!")
+        previous_item = item
+
+    return {
+        "degree_min": head["degree_min"],
+        "degree_max": head["degree_max"],
+        "nm": head["nm"],
+        "t": concatenate([item["t"] for item in sets], axis=0),
+        variable: concatenate([item[variable] for item in sets], axis=1),
+    }
